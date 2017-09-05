598eb434d9f1c7b4425a48b1e3f22bf789a0c21f
meng.fanjun <xmeng.fanjun@contract.isid.co.jp>
2017-09-05 14:22:28 +0900

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,58 @@
+"use strict"
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode"
+import { exec } from "child_process"
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand("extension.openBrowser", () => {
+        const activeTextEditor = vscode.window.activeTextEditor
+        const d = activeTextEditor.document
+        const s = activeTextEditor.selection
+        const selected = d.getText(s)
+        let path: string
+        if (selected.startsWith("https://") || selected.startsWith("http://")) {
+            path = selected
+        } else {
+            path = `https://www.google.com/search?q=${encodeURI(selected)}`
+        }
+        let cmd = open_command(path)
+        exec(cmd, (err, stdout, stderr) => {
+            if (err) {
+                vscode.window.showErrorMessage(`error occured ${err}`)
+            } else {
+                vscode.window.activeTextEditor.selection = new vscode.Selection(
+                    activeTextEditor.selection.end,
+                    activeTextEditor.selection.end
+                )
+            }
+        })
+    })
+
+    context.subscriptions.push(disposable)
+}
+
+function open_command(path: string): string {
+    const platform = process.platform
+    let cmd: string
+    switch (platform) {
+        case "win32":
+            cmd = `start "" "${path}"`
+            break
+        case "darwin":
+            cmd = `open "${path}"`
+            break
+        default:
+            cmd = `xdg-open "${path}"`
+            break
+    }
+    return cmd
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
