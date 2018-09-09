8df176fb1d2920244f0eea50c982679194a49350
anykao <600913@gmail.com>
2018-09-09 22:55:14 +0900

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -8,8 +8,6 @@ const { html_beautify } = require("js-beautify")
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    console.log("mfj plugin is activated.")
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
